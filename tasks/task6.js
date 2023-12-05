e4a636a82400d4ee9457e6d9a6e62a4442140c5a
KyozoKudo <mert.satici@outlook.de>
2023-12-05 20:51:12 +0100

    task 6 klappt

--- tasks/task6.js
+++ tasks/task6.js
@@ -1,24 +1,26 @@
 function merge_obj(arrKeys, arrValues) {
-    var rv = {};
-  for (var i = 0; i < arrKeys.length; ++i)
+    const mergedObject = {};
+  for (let i = 0; i < arrKeys.length; ++i)
   if (arrKeys[i] !== undefined) 
-    rv[arrKeys[i]] = arrValues[i];
+  mergedObject[arrKeys[i]] = arrValues[i];
   
-  return rv;
+  return mergedObject;
 }
   
   
-  function revert_obj(my_dict) {
-    let keys = []
+  function revert_obj(objecto) {
+  const keys = []
     let values = []
-    for (const [key, value] of Object.entries(my_dict)) {
-        keys = key
-        values = value
+    for(const key in objecto) {
+      keys.push(key)
+      values.push(objecto[key])
     }
+
+
+   
     
     return [keys, values]
-    /* Die Funtkion gibt ein 2D Array zurück oder?
-    */
+  
   }
   
   const list1 = ["key2", "key1", "key3"]
@@ -41,8 +43,7 @@ initzalisiert werden. Mal abgesehen davon, dass ich daran zweifle. dass dies üb
 console.log(split_keys.toString())
 console.log(split_values.toString())
 
-let a = [ revert_obj(mo)]
-console.log(a[0].toString)
+
 /* Da ich davon ausgehe, dass ein 2D Array zurückgegeben wird, habe ich es in die Variable gepackt.
 Jetzt dachte ich könne mit den Index auf das erste Array zugreifen und es mit toString ausgeben.
 Funktioniert auch nicht, mir sind die Ideen ausgegangen.
